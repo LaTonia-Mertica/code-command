5c8149fd7be5a5cede9a3e44d0632a0737ba63d9
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-02 21:30:14 -0500

    edited not found page to add 404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -7,8 +7,11 @@ class NotFound extends React.Component {
     render() {
         return (
             <div>
+            <div className="div404"><span className="four04 first4">4</span><span className="four04 only0">0</span><span className="four04 second4">4</span></div>
+
             <h1 class="notFoundPgText">Nothing Here But Code Hopefuls</h1>
             <p class="bottomLine">click around below for interesting extras or click title to return home</p>
+           
             <video src={Glitch} controls="controls" autoPlay loop muted class="notFoundPageVideo" />
             <Footer />
             </div>

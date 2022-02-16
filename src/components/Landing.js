0519dd93d4ce37638b373b071c55603417886650
La'Tonia Mertica <latoniamertica@gmail.com>
2022-02-16 17:47:51 -0500

    video: added to landing page background

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LandingPageBtns from "./LandingPageBtns";
+import scapeSpan from "../components/videos/scapeSpan.mp4";
 import Footer from "./Footer";
 
 const Landing = () => {
@@ -8,6 +9,14 @@ const Landing = () => {
       <LandingPageBtns />
       {/* <h1 id="landingTitle">landing</h1>
         <h2 id="landingTagline">know what's where and why you need to know</h2> */}
+      <video
+        src={scapeSpan}
+        controls="controls"
+        autoPlay
+        loop
+        muted
+        className="notFoundPageVideo staticIntro"
+      />
       <Footer />
     </div>
   );
